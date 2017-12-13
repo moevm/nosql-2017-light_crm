2a4ddf98869850e0de7375d3a33a060955f183c6
dronnikovigor <dronnikovigor@gmail.com>
2017-12-14 00:27:40 +0300

    Clean up

--- src/crm/ArangoCRUD.kt
+++ src/crm/ArangoCRUD.kt
@@ -5,8 +5,6 @@ import com.arangodb.ArangoDB;
 import com.arangodb.ArangoDBException
 import com.arangodb.entity.BaseDocument
 import com.arangodb.entity.CollectionEntity
-import com.arangodb.entity.DocumentEntity
-import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 class ArangoCRUD {
     /*constructor(user: String, password: String) {
